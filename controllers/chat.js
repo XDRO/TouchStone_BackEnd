36e203479679d8102c20bd81ad79bcfceebaaef0
XDRO <joshuaarcher76@gmail.com>
2024-03-25 12:38:42 -0500

    create controllers for generating responses and getting chat history

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,5 +1,44 @@
 // const mongoose = require("mongoose");
-// const validator = require("validator");
-// const user = require("./user");
+const { HttpBadRequest } = require("../utils/err/HttpBadRequest");
+const chat = require("../models/chat");
 
-// const chatItem = new mongoose.Schema({});
+module.exports.generateResponse = async (req, res, next) => {
+  try {
+    const { message, response, createdAt } = req.body;
+    const owner = req.user._id;
+
+    const newChat = await chat.create({
+      owner,
+      message,
+      response,
+      createdAt,
+    });
+
+    const responseData = {
+      _id: newChat._id,
+      message: newChat.message,
+      response: newChat.response,
+      createdAt: newChat.createdAt,
+      owner: newChat.owner,
+    };
+
+    return res.send(responseData);
+  } catch (e) {
+    if (e.name === "ValidationError") {
+      return next(new HttpBadRequest("ValidationError"));
+    }
+    return next(e);
+  }
+};
+
+module.exports.getChatHistory = async (req, res, next) => {
+  try {
+    const chatHistory = await chat.find({});
+    return res.status(200).send(chatHistory);
+  } catch (e) {
+    return next(e);
+  }
+};
+
+// delete item controller
+// and possibly like and dislike controllers
